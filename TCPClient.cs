3c440027ff300c9ee08ec992d37c15ec704c07cf
hugogreendal <100800754+hugogreendal@users.noreply.github.com>
2023-05-24 00:08:45 +0300

    TCP client-server

---
+++ TCPClient.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+#nullable disable
+
+namespace web_tech_lab1
+{
+    public class TCPClient
+    {
+        static async Task Main()
+        {
+            try
+            {
+                Console.WriteLine("Введите имя сервера: ");
+                string serverName = Console.ReadLine();
+                Console.WriteLine("Введите порт для подключения: ");
+                int port = Convert.ToInt32(Console.ReadLine());
+
+                using TcpClient client = new();
+                await client.ConnectAsync(serverName, port);
+                using NetworkStream netStream = client.GetStream();
+
+                while (true)
+                {
+                    
+
+                    // Send some data to the peer.
+                    Console.WriteLine("Введите сообщение (stop для выхода):");
+                    string responce = Console.ReadLine();
+                    byte[] sendBuffer = Encoding.UTF8.GetBytes(responce);
+                    int bytesReceived;
+                    byte[] receiveBuffer = new byte[2048];
+                    
+                    await netStream.WriteAsync(sendBuffer);
+
+                    if (responce == "exit") return;
+
+                    // Receive some data from the peer.
+                    bytesReceived = await netStream.ReadAsync(receiveBuffer);
+                    
+                    string data = Encoding.UTF8.GetString(receiveBuffer.AsSpan(0, bytesReceived));
+                    Console.WriteLine($"This is what the peer sent to you:\n{data}");
+                    if (responce == "stop") return;
+                }
+
+            }
+            catch (SocketException se)
+            {
+                if (se.SocketErrorCode == SocketError.TimedOut)
+                {
+                    Console.WriteLine("Timeout error occurred.");
+                }
+                else if (se.SocketErrorCode == SocketError.HostNotFound)
+                {
+                    Console.WriteLine("Such host does not exist");
+                }
+                else if (se.SocketErrorCode == SocketError.ConnectionRefused)
+                {
+                    Console.WriteLine("Incorrect port");
+                }
+                else
+                {
+                    Console.WriteLine("Socket error occurred: " + se.SocketErrorCode);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+            finally
+            {
+                Console.Write("You've been disconnected.");
+            }
+
+        }
+    }
+}
+
---
+++ TcpServer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class TcpServer
+{
+    private static Boolean isRunning = true;
+    private static int connectionsCount = 0;
+
+    static async Task Main()
+    {
+        int port = 12345;
+        IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+        IPAddress serverIP = ipHostInfo.AddressList[0];
+
+        IPEndPoint serverEndPoint = new IPEndPoint(serverIP, port);
+
+        // Create a TcpListener.
+        TcpListener listener = new TcpListener(serverEndPoint);
+        listener.Start();
+        Console.WriteLine("Server listening on {0}:{1}", serverIP, port);
+
+        try
+        {
+            while (isRunning)
+            {
+                if (listener.Pending())
+                {
+                    // Accept a client connection.
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    Console.WriteLine("Client connected: {0}", client.Client.RemoteEndPoint);
+                    connectionsCount++;
+
+                    // Handle the client connection asynchronously.
+                    _ = HandleClientAsync(client, listener);
+                }
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: {0}", ex);
+        }
+        finally
+        {
+            // Stop listening for new client connections.
+            listener.Stop();
+            Console.WriteLine("Exited");
+        }
+    }
+
+    static async Task HandleClientAsync(TcpClient client, TcpListener server)
+    {
+        string status = "ready";
+        try
+        {
+            // Get a stream object for reading and writing.
+            using NetworkStream stream = client.GetStream();
+            
+
+            while (true)
+            {
+                Stopwatch stopwatch = new();
+                // Read the incoming data.
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received from client: {0}", message);
+                stopwatch.Start();
+
+
+                string response = "";
+                switch (message)
+                {
+                    case "ready\r\n":
+                        if (status == "ready") response = "The server is already working";
+                        else
+                        {
+                            status = "ready";
+                            response = "The server is ready";
+                        }
+                        break;
+                    case "pause\r\n":
+                        if (status == "pause") response = "Server is already paused";
+                        else
+                        {
+                            status = "pause";
+                            response = "The server is paused";
+                        }
+                        break;
+                    case "stop\r\n":
+                        if (status == "pause") response = "Server is paused. Send 'ready' to start.";
+                        else
+                        {
+                            status = "stop";
+                            response = "The server has been stopped by the client";
+                        }
+                        break;
+                    default:
+                        if (status == "pause") response = "Server is paused. Send 'ready' to start.";
+                        else response = "There is no such command";
+                        break;
+
+                }
+
+                if (message == "exit\r\n") return;
+                // Send a response back to the client.
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                Console.WriteLine("Sent to client: {0}", response);
+                stopwatch.Stop();
+                Console.WriteLine($"Время обработки запроса: {stopwatch.Elapsed}.\n");
+                if (message == "stop\r\n" && status != "pause") return;     
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error handling client: {0}", ex);
+        }
+        finally
+        {
+            // Clean up.
+            client.Close();
+            if (status == "stop")
+            {
+                isRunning = false;
+                connectionsCount--;
+            }
+        }
+    }
+}
