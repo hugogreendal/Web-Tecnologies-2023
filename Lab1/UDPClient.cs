119e7256d3febe45799689496427543a089f55d5
hugogreendal <100800754+hugogreendal@users.noreply.github.com>
2023-05-24 00:13:11 +0300

    UDP client-server

---
+++ Lab1/UDPClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Linq;
+
+namespace UPDClient
+{
+    class UPDClient
+    {
+        static void Main()
+        {
+            // Set the server IP address and port number
+            string serverIP = "127.0.0.1";
+            int serverPort = 1234;
+
+            // Create a new UDP client and connect to the server
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+
+            // Create a message to send to the server
+            string message = "Hello, server!";
+
+            // Convert the message to a byte array
+            byte[] byteMessage = Encoding.ASCII.GetBytes(message);
+
+            Packet packet = new Packet();
+            packet.Type = 1;
+            packet.Size = byteMessage.Length;
+            packet.Data = byteMessage;
+
+            byte[] data = packet.ToByteArray();
+
+            // Send the message to the server
+            client.Send(data, data.Length, endPoint);
+
+            // Close the client connection
+            client.Close();
+        }
+    }
+}
+
---
+++ Lab1/UDPServer.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+
+namespace UPDServer
+{
+    class UPDServer
+    {
+        static void Main()
+        {
+            // Set the local IP address and port number to listen on
+            IPAddress localIP = IPAddress.Parse("127.0.0.1");
+            int localPort = 1234;
+
+            // Create a new UDP listener
+            UdpClient listener = new UdpClient(localPort);
+
+            // Create an endpoint to listen for incoming messages
+            IPEndPoint endPoint = new IPEndPoint(localIP, localPort);
+
+            Stopwatch stopwatch = new();
+            try
+            {
+                // Start listening for incoming messages
+                Console.WriteLine("Waiting for messages...");
+                byte[] data = listener.Receive(ref endPoint);
+                stopwatch.Start();
+
+                Packet packet = Packet.FromByteArray(data);
+                int type = packet.Type;
+                int size = packet.Size;
+                byte[] receivedData = packet.Data;
+
+                // Convert the received data to a string
+                string message = Encoding.ASCII.GetString(data);
+
+                // Display the received message
+                Console.WriteLine("Received message: " + message);
+                stopwatch.Stop();
+
+                Console.WriteLine(stopwatch.Elapsed);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                // Close the listener
+                listener.Close();
+            }
+        }
+    }
+}
